b1539fe12890b887b94804e22822c1b530b347d9
Qbeaudiquez <q.beaudiquez.pro@gmail.com>
2024-11-04 18:27:00 +0100

    initialization all pages

--- public/js/scrollbar.js
+++ public/js/scrollbar.js
@@ -3,14 +3,11 @@
 
 const scrollBar = document.createElement("div")
 scrollBar.id = "scrollbar"
-const header = document.querySelector(".header")
-console.log(header)
-header.appendChild(scrollBar)
+const body = document.querySelector("body")
+body.appendChild(scrollBar)
 
 document.addEventListener("scroll", () => {
 
 let heightPourcentage = document.documentElement.scrollTop / (document.body.scrollHeight - window.innerHeight) * 100
 scrollBar.style.width = heightPourcentage + '%'
 })
-
-console.log('hello world')
