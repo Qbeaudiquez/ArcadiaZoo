1c464b18c9ec35aaf37f5ced2f7fb354cc2c7382
Qbeaudiquez <q.beaudiquez.pro@gmail.com>
2024-11-20 12:27:30 +0100

    fix detail display

--- src/insertAnimals.php
+++ src/insertAnimals.php
@@ -2,37 +2,38 @@
     <?php foreach ($habitats as $habitat): ?>
         <?php $habitat["identifier"] = "habitat{$habitat['habitat_id']}"; ?>
 
-        <?php if ($_GET["habitat"] === $habitat["identifier"] || $_GET["habitat"] === "dashbord"): ?>
+        <?php if ($_GET["habitat"] === $habitat["identifier"]): ?>
             <?php foreach ($animals as $animal): ?>
                 <?php if ($animal["habitat_id"] === $habitat['habitat_id']): ?>
                     <div class="cards">
                         <h4 class="animalName">Nom : <?= htmlspecialchars($animal["name"]) ?></h4>
                         <img class="animalImg" src="../../../asset/img_animals/<?= htmlspecialchars($animal["animal_id"]) ?>.png" alt="Image de l'animal">
                         <button class="showDisplay">Clic pour plus de détail</button>
-                        <?php foreach ($reports as $report): ?>
 
-                            <?php if ($report["animal_id"] === $animal['animal_id']): ?>
-                                <?php if ($_GET["habitat"] === $habitat["identifier"]): ?>
-                                    
-                                <?php endif; ?>
-                                
+                        <?php
+                        
+                        $animalReports = array_filter($reports, fn($report) => $report["animal_id"] === $animal["animal_id"]);
+                        ?>
+
+                        <?php if (!empty($animalReports)): ?>
+                            <?php foreach ($animalReports as $report): ?>
                                 <div class="report">
                                     <h4 class="animalRace">Race : <?= htmlspecialchars($animal["race"]) ?></h4>
-                                    <p class="etat">État de l'animal : <?= htmlspecialchars($report["etat"]) ?></p>
+                                    <h4 class="animalHabitat">Habitat : <?= htmlspecialchars($habitat["name"]) ?></h4>
+                                    <h4 class="vetReport">Rapport du vétérinaire</h4>
+                                    <p class="animalEtat">État de l'animal : </p>
+                                    <p class="animalFood">Alimentation : <?= htmlspecialchars($report["food"]) ?></p>
+                                    <p class="animalFoodWeight">Quantité : <?= htmlspecialchars($report["weight"]) ?></p>
                                     <p>Détails : <?= htmlspecialchars($report["detail"]) ?></p>
-
-                                    <?php foreach ($employeReports as $employeReport): ?>
-                                        <?php if ($employeReport["animal_id"] === $animal["animal_id"]): ?>
-                                            <h4 class="reportTitle">Compte rendu Employé(e)</h4>
-                                            <p>Dernier repas : <?= htmlspecialchars($employeReport["last_meal"]) ?></p>
-                                            <p>Date Dernier repas : <?= htmlspecialchars($employeReport["last_passage"]) ?></p>
-                                        <?php endif; ?>
-                                        
-                                    <?php endforeach; ?>
-                                    
                                 </div>
-                            <?php endif; ?>
-                        <?php endforeach; ?>
+                            <?php endforeach; ?>
+                        <?php else: ?>
+                            <div class="report">
+                                <h4 class="animalRace">Race : <?= htmlspecialchars($animal["race"]) ?></h4>
+                                <h4 class="animalHabitat">Habitat : <?= htmlspecialchars($habitat["name"]) ?></h4>
+                                <p class="vetReport">Aucun rapport vétérinaire disponible pour le moment.</p>
+                            </div>
+                        <?php endif; ?>
                     </div>
                 <?php endif; ?>
             <?php endforeach; ?>
