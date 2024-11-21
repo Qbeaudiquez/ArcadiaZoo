3132568c362a9ed99f0b16a2973c1e2ac36d1187
Quentin <quentin.beaudiquez@gmail.com>
2024-11-21 10:13:10 +0100

    init heroku database

--- config/mysql.php
+++ config/mysql.php
@@ -1,5 +1,15 @@
 <?php
 
+if(getenv('JAWSDB_URL') !== false){
+    $url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
+}
+
 const MYSQL_HOST = 'localhost';
 const MYSQL_PORT = 3306;
 const MYSQL_NAME = 'zoo_arcadia';
